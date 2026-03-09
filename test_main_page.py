690e7a72d9b489aee0ef0c35d80094fd75fa54c5
quantos <100156933+quant0s@users.noreply.github.com>
2026-03-09 23:00:04 +0300

    check alert

--- test_main_page.py
+++ test_main_page.py
@@ -15,10 +15,3 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-def test_guest_have_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()

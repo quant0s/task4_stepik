72706b3de9b0cef4a9efaf1603455e0fbaeba597
quantos <100156933+quant0s@users.noreply.github.com>
2026-03-10 12:58:51 +0300

    add test login page from product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,8 @@ from selenium.common.exceptions import NoSuchElementException, NoAlertPresentExc
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+from .locators import BasePageLocators
+
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -13,6 +15,13 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_element_present(self, how, what):
         try:
